342c24e1a810698bbc304b1e56f4698cfeb3eb98
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-04 21:22:11 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,11 @@
 class Course
 
-  attr_accessor :title, :schedule, :description, 
+  attr_accessor :title, :schedule, :description,
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
 
 end
